9a7366c20d5df80fd4878dc56a714b292a7a3a3d
d-ben-b <155895903+d-ben-b@users.noreply.github.com>
2025-08-11 19:11:22 +0800

    Refactor searchRange function: implement binary search for finding first and last positions of target in sorted array

--- 34/Find First and Last Position of Element in Sorted Array.py
+++ 34/Find First and Last Position of Element in Sorted Array.py
@@ -3,26 +3,35 @@ from typing import List
 
 def searchRange(nums: List[int], target: int) -> List[int]:
     l, r = 0, len(nums) - 1
-
-    # handle edge cases
     if len(nums) == 0:
         return [-1, -1]
+    low = binSearch(nums, target, low=True)
+    if low == -1:
+        return [low, low]
+    right = binSearch(nums, target, low=False)
+    return [low, right]
 
-    # main method
-    while l < r:
-        # print(f"{l=} {r=}")
-        if nums[l] == target and nums[r] == target:
-            return [l, r]
-        if nums[l] < target:
-            l += 1
-        if nums[r] > target:
-            r -= 1
-    # for some case that number only appears once
-    if nums[l] == target:
-        return [l, l]
-    elif nums[r] == target:
-        return [r, r]
-    return [-1, -1]
+
+def binSearch(arr, tar, low) -> int:
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] == tar:
+            if low:
+                if l == m or arr[m - 1] < tar:
+                    return m
+                else:
+                    r = m - 1
+            else:
+                if r == m or arr[m + 1] > tar:
+                    return m
+                else:
+                    l = m + 1
+        elif arr[m] > tar:
+            r = m - 1
+        else:
+            l = m + 1
+    return -1
 
 
 print(searchRange([5, 7, 7, 8, 8, 10], 8))  # [3, 4]
